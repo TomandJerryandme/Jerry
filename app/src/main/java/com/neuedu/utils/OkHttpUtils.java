42295ba5e823c0594ce045994784da5b47872934
TomandJerry <1728703711@qq.com>
2019-12-20 09:42:22 +0800

    Android项目

---
+++ app/src/main/java/com/neuedu/utils/OkHttpUtils.java
@@ -0,0 +1,14 @@
+package com.neuedu.utils;
+
+
+import okhttp3.OkHttpClient;
+
+public class OkHttpUtils {
+
+    private static final OkHttpClient CLIENT = new OkHttpClient();
+
+
+    public static void get(String url, OkHttpCallback callback){
+        
+    }
+}
